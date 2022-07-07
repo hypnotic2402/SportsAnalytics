31bacef5c36ae27dc859d6b9c9119b93ab7b187f
hypnotic2402 <Saket.Gurjar@iiitb.ac.in>
2022-07-07 08:37:04 +0530

    Dist matrix p2

--- ShotClassifier/model.py
+++ ShotClassifier/model.py
@@ -7,6 +7,10 @@ def fillDataSet(ds): #function to fill the data set
 def getTestData(td): #function to get the test data
     pass
 
+def normalizeArray(ar , mean , var): #function to normalize the array to zero mean and unit standard deviation
+    ar = (ar - mean) / var
+    return ar
+
 def generateDistanceMatrix(distMat , td , ds):
     
     for x in ds: # r such iterations
@@ -15,18 +19,30 @@ def generateDistanceMatrix(distMat , td , ds):
         n = multiDimSignal[0].length()
 
         meanVar = []
-        for i in range(l):
-            meanVar.append(0)
+        # for i in range(l):
+        #     meanVar.append([0,0])
 
         for i in multiDimSignal.length():
             arr = np.array(multiDimSignal[i])
             meanVar.append([np.mean(arr) , np.std(arr)])
 
+        temp = np.empty([l , n])
+        for i in range(l):
+            temp.append((np.array(multiDimSignal[i]) - meanVar[i,0]) / meanVar[i,1]) 
+
+        # temp now holds normalized vlues of each feature's vector
+
+        # Smoothen with gaussian if neccessary later
+
+
+
+        
+
+
+
+
         
             
-
-
-
     
 
 def calcDTWdistance(distMat): #function to retrun the DTW allignment cost
@@ -39,13 +55,19 @@ def calcDTWdistance(distMat): #function to retrun the DTW allignment cost
 
 # type = 0 , 1 , 2 or 3
 
+
+
 dataSet = [] # [ [ [n X l Matrix] , type] , [[n X l Matrix] , type] , ... r times] : [n X l Matrix] = [[feature0 array] , [feature1 array] , [feature2 array] , ...[featurel array]]
 fillDataSet(dataSet)
 
 testData = [] # [m X l Matrix]
 getTestData(testData)
 
-distMat = [] # [ [n X m Matrix] , [n X m Matrix] , [n X m Matrix] , ... r times]
+l = dataSet.length()
+n = dataSet[0].length()
+
+
+distMat = np.zeros([]) # [ [n X m Matrix] , [n X m Matrix] , [n X m Matrix] , ... r times]
 generateDistanceMatrix(distMat, testData , dataSet)
 
 costData = [] # r long array
