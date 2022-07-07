a2ff26d8bdcda11bedac00e5b117db91b110335a
hypnotic2402 <Saket.Gurjar@iiitb.ac.in>
2022-07-07 07:36:34 +0530

    Dist matrix p1

--- ShotClassifier/model.py
+++ ShotClassifier/model.py
@@ -1,4 +1,5 @@
-
+from unicodedata import normalize
+import numpy as np
 
 def fillDataSet(ds): #function to fill the data set
     pass
@@ -7,7 +8,26 @@ def getTestData(td): #function to get the test data
     pass
 
 def generateDistanceMatrix(distMat , td , ds):
-    pass
+    
+    for x in ds: # r such iterations
+        multiDimSignal = x[0]
+        l = multiDimSignal.length()
+        n = multiDimSignal[0].length()
+
+        meanVar = []
+        for i in range(l):
+            meanVar.append(0)
+
+        for i in multiDimSignal.length():
+            arr = np.array(multiDimSignal[i])
+            meanVar.append([np.mean(arr) , np.std(arr)])
+
+        
+            
+
+
+
+    
 
 def calcDTWdistance(distMat): #function to retrun the DTW allignment cost
     return 0
@@ -19,7 +39,7 @@ def calcDTWdistance(distMat): #function to retrun the DTW allignment cost
 
 # type = 0 , 1 , 2 or 3
 
-dataSet = [] # [ [ [n X l Matrix] , type] , [[n X l Matrix] , type] , ... r times]
+dataSet = [] # [ [ [n X l Matrix] , type] , [[n X l Matrix] , type] , ... r times] : [n X l Matrix] = [[feature0 array] , [feature1 array] , [feature2 array] , ...[featurel array]]
 fillDataSet(dataSet)
 
 testData = [] # [m X l Matrix]
