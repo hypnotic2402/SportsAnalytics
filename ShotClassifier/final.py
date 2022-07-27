2993ed43b6b8bceb4eacb6bc745ca7446a78354c
hypnotic2402 <Saket.Gurjar@iiitb.ac.in>
2022-07-27 17:52:24 +0530

    hyperParam tuning

--- ShotClassifier/final.py
+++ ShotClassifier/final.py
@@ -5,7 +5,7 @@ import subprocess
 x = {}
 
 for k in range(100):
-    
+    # k = 10
     wrong = 0
     correct = 0
 
@@ -13,19 +13,22 @@ for k in range(100):
         for j in range(1,5):
             fn = "p" + str(i) + "-s"+str(j) + "-v1.csv" 
             # result = os.popen("python3 model.py " + "p" + str(i) + "-s"+str(j) +"-v1.csv")
-            result = str(subprocess.run(["python3" , "model.py" , fn , str(k)] , capture_output = True , text=True))
-            # CompletedProcess(args=['python3', 'model.py', 'p1-s1-v1.csv'], returncode=0, stdout=b'1\n', stderr=b'')
-            result = result[90:91]
+            result = subprocess.run(["python3" , "model.py" , fn , str(k)] , capture_output = True , text=True).stdout
+            result = str(result)
+            # CompletedProcess(args=['python3', 'model.py', 'p1-s1-v1.csv' , "3"], returncode=0, stdout=b'1\n', stderr=b'')
+            # result = result[90:91]
 
-            # print(result)
-            if str(result) == str(0):
+            # print(str(int(result)))
+            if str(int(result)) == str(0):
+                # print("wrong")
                 wrong += 1
-            else:
+            elif str(int(result)) == str(1):
+                # print("correct")
                 correct += 1
 
     print()
-    # print("Correct = " + str(correct))
-    # print("Wrong = " + str(wrong))
+    print("Correct = " + str(correct))
+    print("Wrong = " + str(wrong))
 
     accuracy = str(100 * correct/(correct + wrong))
     print("k = " + str(k) + " : Accuracy = " + accuracy)
--- ShotClassifier/model.py
+++ ShotClassifier/model.py
@@ -203,7 +203,7 @@ costSorted = {k: v for k, v in sorted(costs.items(), key=lambda item: item[1])}
 
 
 
-k = sys.argv[2]  # number of k neighbors to be considered
+k = int(sys.argv[2])  # number of k neighbors to be considered
 z = 0
 indexes = []
 for i in costSorted:
